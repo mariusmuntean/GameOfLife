190e7973667c7b65ad6d4b49d70c48834638982b
Marius M <marius.muntean@live.com>
2021-09-26 08:59:48 +0200

    Added a simple VM with Life instance

---
+++ gol.maui/gol.maui/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,33 @@
+﻿using gol.maui.Models;
+using Microsoft.Maui.Controls;
+
+namespace gol.maui.ViewModels
+{
+    public class MainPageViewModel : BindableObject
+    {
+        private Life life;
+
+        public MainPageViewModel()
+        {
+            Life = new Life(30, 40);
+
+            // Glider
+            Life.Toggle(2, 2);
+            Life.Toggle(3, 2);
+            Life.Toggle(4, 2);
+            Life.Toggle(4, 1);
+            Life.Toggle(3, 0);
+        }
+
+        public Life Life
+        {
+            get => life;
+            set
+            {
+                life = value;
+                OnPropertyChanged();
+            }
+        }
+
+    }
+}
